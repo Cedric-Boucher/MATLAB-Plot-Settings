fa61ae7b21dd7f5f929671eb9d72121f12872f88
Cedric Boucher <onebitalpha@gmail.com>
2023-06-19 11:19:58 -0600

    docstring correction

--- MATLAB_Plot_Settings.m
+++ MATLAB_Plot_Settings.m
@@ -2,7 +2,7 @@ function MATLAB_Plot_Settings(figure, legend, axes)
     %{
         to get figure you can use gcf or you can use <variable> = figure()
         to get axes you can use gca or you can use <variable> = axes()
-        to get legend you can use <variable> = legend()
+        to get legend you can use <variable> = legend(), or you can set legend to 0
     %}
 
     set(axes, 'TickLength', [0.0075 0.025], 'TickDir', 'out', 'XColor', 'black', 'Box', 'on', 'FontUnits', 'points', 'FontName', 'Maven Pro', 'FontSize', 16,'LabelFontSizeMultiplier',1, 'YColor', 'black');
